b88622b90f6868c43eea7e62684c430b1a869569
nitingite16588 <nitinresume16588@gmail.com>
2020-10-12 19:07:01 +0530

    router added to burger project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {Route ,Switch} from 'react-router-dom'
+import {Route ,Switch} from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
+ 
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/"  exact component={BurgerBuilder} />
+          <Route path="/orders"  exact component={Orders} />
         </Switch>
       </Layout>
 
