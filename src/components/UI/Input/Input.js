423c8768406410474bf75937ca4f8d025f642cda
nitingite16588 <nitinresume16588@gmail.com>
2020-10-13 17:21:15 +0530

    forrm validation completed

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Input.css';
+const input = (props) => {
+
+    let inputElement = null;
+    let inputClasses = "InputElement";
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses+= "  Invalid";
+    }
+
+    switch (props.elementType) {
+        case ('input'):
+            inputElement = <input className={inputClasses } {...props.elementConfig} value={props.value} 
+            onChange={props.changed}/>;
+            break;
+        case ('textarea'):
+            inputElement = <textarea className={inputClasses } {...props.elementConfig} value={props.value} 
+            onChange={props.changed}/>;
+            break;
+
+        case ('select'):
+            inputElement = (<select 
+                className={inputClasses }
+             value={props.value} 
+             onChange={props.changed}>
+                 {props.elementConfig.options.map(option=>(
+                     <option key= {option.value}value={option.value}>{option.displayValue} </option>
+                 ))}
+            </select>)
+            break;
+        default:
+            inputElement = <input className="InputElement" {...props.elementConfig} value={props.value} />;
+    }
+
+
+    return (
+        <div className="Input">
+            <label className="Label">{props.label}</label>
+            {inputElement}
+        </div>
+    )
+}
+
+export default input
